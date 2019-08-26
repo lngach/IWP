d3f31395b7f7800b5d23aa9d51679c7e44068d77
lngach <nam.le@gumiviet.com>
2019-08-26 17:27:16 +0700

    Change chrome maximum mode to full screen mode

--- classes/iw.py
+++ classes/iw.py
@@ -16,8 +16,8 @@ from selenium.webdriver.chrome.options import Options
 
 
 class InteractiveWall():
-    _mBGMId = ''
-    _mMaxMovieCount = 3
+    _m_bgm_id = ''
+    _m_max_movie_count = 3
     DEPTH_SENSOR_WIDTH = 640
     DEPTH_SENSOR_HEIGHT = 480
     DEFAULT_MAX_MOVIE_COUNT = 3
@@ -31,23 +31,23 @@ class InteractiveWall():
     AUDIO_STOP_KEY = '_stop'
     AUDIO_PAUSE_KEY = '_pause'
     CHROMEDRIVER_PATH = '/home/lngach/Workplace/interactive-wall/chromedriver'
-    _mContentPath = None
-    _mDeviceList = None
-    _mContext = None
-    _mIsUpdatingDeviceList = None
-    _mMovieList = None
-    _mDeviceSetting = None
-    _mHitAreaIdList = None
-    _mHitList = None
-    _mCurrentHitList = None
-    _mLastHitTime = None
+    _m_content_path = None
+    _m_device_list = None
+    _m_context = None
+    _m_is_updating_device_list = None
+    _m_movie_list = None
+    _m_device_setting = None
+    _m_hit_area_list = None
+    _m_hit_list = None
+    _m_current_hit_list = None
+    _m_last_hit_time = None
 
     @staticmethod
     def run():
-        InteractiveWall._mIsUpdatingDeviceList = False
-        InteractiveWall._mDeviceList = []
-        InteractiveWall._mDeviceSetting = []
-        InteractiveWall._mMovieList = []
+        InteractiveWall._m_is_updating_device_list = False
+        InteractiveWall._m_device_list = []
+        InteractiveWall._m_device_setting = []
+        InteractiveWall._m_movie_list = []
 
         try:
             with open('/home/lngach/Workplace/interactive-wall/setting.txt', 'r') as f:
@@ -63,7 +63,7 @@ class InteractiveWall():
                                     if not(str == "wait_movie"):
                                         if not(str == "movie"):
                                             if str == "bgm":
-                                                InteractiveWall._mBGMId = str_array[1].strip(
+                                                InteractiveWall._m_bgm_id = str_array[1].strip(
                                                 )
                                         else:
                                             str_array2 = str_array[1].strip().split(
@@ -77,7 +77,7 @@ class InteractiveWall():
                                                     hit_area.top = str_array2[2]
                                                     hit_area.right = str_array2[3]
                                                     hit_area.bottom = str_array2[4]
-                                                    InteractiveWall._mMovieList.append(
+                                                    InteractiveWall._m_movie_list.append(
                                                         hit_area)
                                                 except Exception as e:
                                                     print(
@@ -93,7 +93,7 @@ class InteractiveWall():
                                             try:
                                                 wait_movie_data.wait_time = str_array2[1].strip(
                                                 )
-                                                InteractiveWall._mMovieList.append(
+                                                InteractiveWall._m_movie_list.append(
                                                     wait_movie_data)
                                             except Exception as e:
                                                 print(
@@ -118,56 +118,56 @@ class InteractiveWall():
                                             )
                                             device_setting_info.offset_top = str_array2[6].strip(
                                             )
-                                            InteractiveWall._mDeviceSetting.append(
+                                            InteractiveWall._m_device_setting.append(
                                                 device_setting_info)
                                         except Exception as e:
                                             print(
                                                 'Error at third block {0}'.format(e))
                             else:
                                 try:
-                                    InteractiveWall._mMaxMovieCount = str_array[1].strip(
+                                    InteractiveWall._m_max_movie_count = str_array[1].strip(
                                     )
                                 except Exception as e:
                                     print(
                                         'Error at second block {0}'.format(e))
 
                         else:
-                            InteractiveWall._mContentPath = str_array[1].strip(
+                            InteractiveWall._m_content_path = str_array[1].strip(
                             )
                     line = f.readline()
-            InteractiveWall._mContext = rs.context()
-            InteractiveWall._mContext.set_devices_changed_callback(
+            InteractiveWall._m_context = rs.context()
+            InteractiveWall._m_context.set_devices_changed_callback(
                 InteractiveWall.on_sense_devices_changed)
             InteractiveWall.update_device_list()
-            if len(InteractiveWall._mDeviceSetting) <= 0:
+            if len(InteractiveWall._m_device_setting) <= 0:
                 print("No devices are connected")
             else:
                 print("Devices are connected")
-                InteractiveWall._mHitAreaIdList = []
-                InteractiveWall._mHitList = []
-                InteractiveWall._mCurrentHitList = []
-                for i in range(len(InteractiveWall._mMovieList)):
-                    mMovie = InteractiveWall._mMovieList[i]
-                    InteractiveWall._mHitList.append(False)
-                    InteractiveWall._mCurrentHitList.append(False)
+                InteractiveWall._m_hit_area_list = []
+                InteractiveWall._m_hit_list = []
+                InteractiveWall._m_current_hit_list = []
+                for i in range(len(InteractiveWall._m_movie_list)):
+                    mMovie = InteractiveWall._m_movie_list[i]
+                    InteractiveWall._m_hit_list.append(False)
+                    InteractiveWall._m_current_hit_list.append(False)
                     if isinstance(mMovie, HitArea):
-                        InteractiveWall._mHitAreaIdList.append(i)
-                InteractiveWall._mLastHitTime = time.time()
+                        InteractiveWall._m_hit_area_list.append(i)
+                InteractiveWall._m_last_hit_time = time.time()
 
                 options = Options()
-                options.add_argument('disable-infobars')
+                options.add_argument('--kiosk')
                 driver = webdriver.Chrome(
                     InteractiveWall.CHROMEDRIVER_PATH, chrome_options=options)
-                driver.get('file://' + InteractiveWall._mContentPath)
-                if InteractiveWall._mBGMId != "":
+                driver.get('file://' + InteractiveWall._m_content_path)
+                if InteractiveWall._m_bgm_id != "":
                     driver.find_element_by_id(
-                        InteractiveWall._mBGMId + "_play").click()
+                        InteractiveWall._m_bgm_id + "_play").click()
 
                 while True:
-                    for _, current in enumerate(InteractiveWall._mHitAreaIdList):
-                        InteractiveWall._mCurrentHitList[current] = False
-                    for _, current in enumerate(InteractiveWall._mDeviceSetting):
-                        _mDeviceDict = dict(InteractiveWall._mDeviceList)
+                    for _, current in enumerate(InteractiveWall._m_hit_area_list):
+                        InteractiveWall._m_current_hit_list[current] = False
+                    for _, current in enumerate(InteractiveWall._m_device_setting):
+                        _mDeviceDict = dict(InteractiveWall._m_device_list)
                         if current.device_serial in _mDeviceDict:
                             mDevice = _mDeviceDict[current.device_serial]
                             if mDevice.frame_wait(current.touch_line) and mDevice.is_updated_frame_buffer():
@@ -183,19 +183,19 @@ class InteractiveWall():
                                         num5 = num3 * \
                                             sin(num2*pi / 180.0) + \
                                             float(current.offset_top)
-                                        for j, item in enumerate(InteractiveWall._mHitAreaIdList):
-                                            if not InteractiveWall._mCurrentHitList[item]:
-                                                mMovie = InteractiveWall._mMovieList[item]
+                                        for j, item in enumerate(InteractiveWall._m_hit_area_list):
+                                            if not InteractiveWall._m_current_hit_list[item]:
+                                                mMovie = InteractiveWall._m_movie_list[item]
                                                 if float(mMovie.left) <= float(num4) and float(mMovie.top) <= float(num5) and (float(num4) <= float(mMovie.right) and float(num5) <= float(mMovie.bottom)):
-                                                    InteractiveWall._mCurrentHitList[j] = True
+                                                    InteractiveWall._m_current_hit_list[j] = True
                     flag = False
                     num = 0
-                    for _, current in enumerate(InteractiveWall._mHitAreaIdList):
-                        if InteractiveWall._mCurrentHitList[current]:
+                    for _, current in enumerate(InteractiveWall._m_hit_area_list):
+                        if InteractiveWall._m_current_hit_list[current]:
                             flag = True
                     if flag:
-                        for i in range(len(InteractiveWall._mMovieList)):
-                            mMovie = InteractiveWall._mMovieList[i]
+                        for i in range(len(InteractiveWall._m_movie_list)):
+                            mMovie = InteractiveWall._m_movie_list[i]
                             if isinstance(mMovie, WaitMovieData):
                                 if driver.find_element_by_id(mMovie.movie_status_key()).get_attribute("data_is_play") == "true":
                                     driver.find_element_by_id(
@@ -203,23 +203,23 @@ class InteractiveWall():
                             elif driver.find_element_by_id(mMovie.movie_status_key()).get_attribute("data_is_play") == "true":
                                 ++num
                     else:
-                        time_span = time.time() - InteractiveWall._mLastHitTime
-                        for i in range(len(InteractiveWall._mMovieList)):
-                            mMovie = InteractiveWall._mMovieList[i]
+                        time_span = time.time() - InteractiveWall._m_last_hit_time
+                        for i in range(len(InteractiveWall._m_movie_list)):
+                            mMovie = InteractiveWall._m_movie_list[i]
                             if isinstance(mMovie, WaitMovieData) and time_span >= float(mMovie.wait_time) and not driver.find_element_by_id(mMovie.movie_status_key()).get_attribute("data_is_play") == "true":
                                 driver.find_element_by_id(
                                     mMovie.movie_start_key()).click()
-                    for _, current in enumerate(InteractiveWall._mHitAreaIdList):
-                        if InteractiveWall._mCurrentHitList[current] != InteractiveWall._mHitList[current]:
-                            mMovie = InteractiveWall._mMovieList[current]
-                            InteractiveWall._mHitList[current] = InteractiveWall._mCurrentHitList[current]
-                            if num < int(InteractiveWall._mMaxMovieCount) and InteractiveWall._mHitList[current] and not driver.find_element_by_id(mMovie.movie_status_key()).get_attribute("data_is_play") == "true":
+                    for _, current in enumerate(InteractiveWall._m_hit_area_list):
+                        if InteractiveWall._m_current_hit_list[current] != InteractiveWall._m_hit_list[current]:
+                            mMovie = InteractiveWall._m_movie_list[current]
+                            InteractiveWall._m_hit_list[current] = InteractiveWall._m_current_hit_list[current]
+                            if num < int(InteractiveWall._m_max_movie_count) and InteractiveWall._m_hit_list[current] and not driver.find_element_by_id(mMovie.movie_status_key()).get_attribute("data_is_play") == "true":
                                 driver.find_element_by_id(
                                     mMovie.movie_start_key()).click()
                                 ++num
         except Exception as e:
             # print("Error at the first block {0}".format(e))
-            print("IW has stoped")
+            print("IW has stopped")
 
     @staticmethod
     def on_sense_devices_changed(removed, added):
@@ -227,14 +227,14 @@ class InteractiveWall():
 
     @staticmethod
     def update_device_list():
-        InteractiveWall._mIsUpdatingDeviceList = True
-        InteractiveWall._mDeviceList.clear()
-        for _, current in enumerate(InteractiveWall._mContext.devices):
+        InteractiveWall._m_is_updating_device_list = True
+        InteractiveWall._m_device_list.clear()
+        for _, current in enumerate(InteractiveWall._m_context.devices):
             device_data = DeviceData(
                 current, InteractiveWall.DEPTH_SENSOR_WIDTH, InteractiveWall.DEPTH_SENSOR_HEIGHT)
             key = device_data.device.get_info(rs.camera_info.serial_number)
-            InteractiveWall._mDeviceList.append((key, device_data))
+            InteractiveWall._m_device_list.append((key, device_data))
 
-        for _, current in enumerate(InteractiveWall._mDeviceList):
+        for _, current in enumerate(InteractiveWall._m_device_list):
             print(" >> Device serial {0}".format(current))
-        InteractiveWall._mIsUpdatingDeviceList = False
+        InteractiveWall._m_is_updating_device_list = False
